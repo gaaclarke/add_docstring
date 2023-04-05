7ecb2c209f12fd5bf4b1daa4f53f89b0fe6828ba
Aaron Clarke <aaclarke@google.com>
2023-04-05 10:50:17 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# add_docstring
+
+Pipes a dart source file procedure-by-procedure to OpenAI's LLM to generate
+docstrings for the procedure and writes out the new code.
---
+++ test/add_docstring_test.dart
@@ -0,0 +1,8 @@
+import 'package:add_docstring/add_docstring.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
